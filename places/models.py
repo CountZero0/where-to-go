2432ab9906ff725a91a25d667366d1b56c7ffa59
Konstantin <akechijinsai@gmail.com>
2022-02-12 23:34:05 +0300

    Add two places to db

--- places/models.py
+++ places/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-class PLace(models.Model):
-    pass
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    description_short = models.TextField(null=True)
+    description_long = models.TextField(null=True)
+    lng = models.FloatField(null=True)
+    lat = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.title
