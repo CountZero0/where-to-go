bcdfc871b359d11ddc912997677cc93b58abfb21
Konstantin <akechijinsai@gmail.com>
2022-02-13 13:05:27 +0300

    Add image preview to Place inline admin UI

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from places.models import Image, Place
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    ordering = ('place', 'position')
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ['image', 'get_preview', 'position']
     extra = 1
+    readonly_fields = ('get_preview',)
+
+    def get_preview(self, preview_image):
+        return mark_safe(
+            f'<img src="{preview_image.image.url}" width=300 height=200 />')
 
 
 @admin.register(Place)
@@ -12,6 +25,3 @@ class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline
     ]
-
-
-admin.site.register(Image)
