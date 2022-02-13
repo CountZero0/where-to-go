e06bc0c9bd7e2fc59361aa41c8f9e0e0136b6fb8
Konstantin <akechijinsai@gmail.com>
2022-02-13 18:38:34 +0300

    Fix environment variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+ALLOWED_HOSTS = []
 
 SECRET_KEY = env.str(
     "SECRET_KEY",
